9301e530c73e442f35f3b154c7dde1d62777181e
gabrielrodris <biel790832157@gmail.com>
2025-09-02 16:21:39 -0300

    criando back end

---
+++ agendaBackEnd/src/main/java/com/example/agendaBackEnd/AgendaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.agendaBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaBackEndApplication.class, args);
+	}
+
+}
---
+++ agendaBackEnd/src/test/java/com/example/agendaBackEnd/AgendaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agendaBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
