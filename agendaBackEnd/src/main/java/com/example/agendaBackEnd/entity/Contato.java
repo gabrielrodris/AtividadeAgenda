d4c5b68cdb6eb3c36431bdbbb94aca82c3f931f8
CarlosGes <ca.evangelista99@gmail.com>
2025-09-02 17:22:12 -0300

    atributos contato adicionado

--- agendaBackEnd/src/main/java/com/example/agendaBackEnd/entity/Contato.java
+++ agendaBackEnd/src/main/java/com/example/agendaBackEnd/entity/Contato.java
@@ -1,4 +1,31 @@
 package com.example.agendaBackEnd.entity;
 
 public class Contato {
+    private String nome;
+    private String telefone;
+    private String emaiil;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmaiil() {
+        return emaiil;
+    }
+
+    public void setEmaiil(String emaiil) {
+        this.emaiil = emaiil;
+    }
 }
